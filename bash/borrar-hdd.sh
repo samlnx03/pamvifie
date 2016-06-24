b1cb2c4f4de890bca9290bf6a0712af92d57b1b6
samlnx03 <samlnx03@hotmail.com>
2016-06-24 18:16:46 -0500

    Create borrar-hdd.sh

---
+++ bash/borrar-hdd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+NAME=$1
+if [ -z "$NAME" ]; then
+	echo "-1 no se paso argumento 1 nombre"
+	exit
+fi
+
+exec 2>/dev/null
+
+#sperez@fiesrv:~/pamvifie$ sudo lvcreate -L 6G -n MOI5 fie_vg
+#  Logical volume "MOI5" already exists in volume group "fie_vg"
+#sperez@fiesrv:~/pamvifie$ sudo lvcreate -L 100M -n borrame fie_vg
+#  Logical volume "borrame" created
+#sperez@fiesrv:~/pamvifie$ sudo lvremove -f fie_vg/borrame1
+#  Logical volume "borrame1" successfully removed
+
+S="sudo lvcreate -L $SIZE -n $NAME fie_vg"
+echo "$S"
