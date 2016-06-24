6bf529a57540ccd0bce145f3754b2402a499583b
samlnx03 <samlnx03@hotmail.com>
2016-06-24 18:45:32 -0500

    Update mv-corriendo.sh

--- bash/mv-corriendo.sh
+++ bash/mv-corriendo.sh
@@ -5,7 +5,7 @@ TFILE="$(mktemp)"
 ps ax | grep qemu-system-x86_64 | grep -i -e 'name [a-z0-9_-]* ' -o | awk '{print $2}' > $TFILE
 
 # quitar las de la lista negra
-for f in $(/home/sperez/pamvifie/lista-negra.sh); do
+for f in $(/home/sperez/pamvifie/mv-lista-negra.sh); do
 	sed -i "/$f/d" $TFILE
 done
 cat $TFILE
