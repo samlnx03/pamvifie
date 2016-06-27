d18c90c61a0ebf21168a3f5935c34e72e8ac3a6f
samlnx03 <samlnx03@gmail.com>
2016-06-27 16:32:08 -0500

    limpiando

--- mv-lista-negra.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#maquinas virtuales definidas por virsh
-
-/usr/bin/virsh list --name | grep  -v "^$"
-
--- pamvifie/bash/lista-isos.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#todas las imagenes iso para instalar y otras cosas
-
-virsh vol-list default | grep iso | awk '{print $1}'
